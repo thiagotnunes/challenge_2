dc147b20b07d78cfc2333d5c390e51735b4b86b7
Thiago Nunes <thiagotnunes@gmail.com>
2017-10-25 20:53:35 +0200

    Refactors

--- src/main/scala/com/n26/challenge/handlers/StatisticsHandler.scala
+++ src/main/scala/com/n26/challenge/handlers/StatisticsHandler.scala
@@ -1,5 +1,6 @@
 package com.n26.challenge.handlers
 
+import com.n26.challenge.models.Statistics
 import com.n26.challenge.repositories.StatisticsRepository
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response, Status}
@@ -8,10 +9,13 @@ import play.api.libs.json.Json
 
 class StatisticsHandler(repository: StatisticsRepository) extends Service[Request, Response] {
   override def apply(request: Request): Future[Response] = {
-    val stats = repository.getStatistics()
-    val response = Response(request.version, Status.Ok)
+    Future.value(jsonResponseFrom(repository.getStatistics()))
+  }
+
+  private def jsonResponseFrom(stats: Statistics): Response = {
+    val response = Response(Status.Ok)
     response.contentString = Json.stringify(Json.toJson(stats))
     response.contentType = "application/json"
-    Future.value(response)
+    response
   }
 }
